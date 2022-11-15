05abb9c11a6f298684cf759362ad80668a4e5230
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-11-15 18:05:08 +0900

    플레이어의 타입에 맞게 움직이도록 구현

--- Game/Source/PlayerInputComponent.cpp
+++ Game/Source/PlayerInputComponent.cpp
@@ -1,27 +1,43 @@
 #include "PlayerInputComponent.h"
 #include "Body.h"
+#include "Player.h"
 #include "GameObject.h"
 #include "Input.h"
 
 void PlayerInputComponent::Tick(Input& InInput)
 {
+	Player* CurrentPlayer = reinterpret_cast<Player*>(Object_);
+
 	Body& ObjectBody = Object_->GetBody();
 	float Rotate = ObjectBody.GetRotate();
 
-	if (InInput.GetKeyPressState(EScanCode::CODE_UP) == EPressState::PRESSED)
+	EScanCode UpCode, DownCode;
+
+	if (CurrentPlayer->GetType() == Player::EType::PLAYER1)
+	{
+		UpCode = EScanCode::CODE_Q;
+		DownCode = EScanCode::CODE_A;
+	}
+	else // CurrentPlayer->GetType() == Player::EType::PLAYER2
+	{
+		UpCode = EScanCode::CODE_UP;
+		DownCode = EScanCode::CODE_DOWN;
+	}
+
+	if (InInput.GetKeyPressState(UpCode) == EPressState::PRESSED)
 	{
 		Rotate = 270.0f;
 	}
 
-	if (InInput.GetKeyPressState(EScanCode::CODE_DOWN) == EPressState::PRESSED)
+	if (InInput.GetKeyPressState(DownCode) == EPressState::PRESSED)
 	{
 		Rotate = 90.0f;
 	}
 
 	ObjectBody.SetRotate(Rotate);
 
-	if (InInput.GetKeyPressState(EScanCode::CODE_UP) == EPressState::HELD ||
-		InInput.GetKeyPressState(EScanCode::CODE_DOWN) == EPressState::HELD)
+	if (InInput.GetKeyPressState(UpCode) == EPressState::HELD ||
+		InInput.GetKeyPressState(DownCode) == EPressState::HELD)
 	{
 		ObjectBody.SetCanMove(true);
 	}
