2d3a10559c60c45c36beb02692f7775086f950e4
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-11-19 08:08:30 +0900

    누락된 폴더 변경 사항 적용

--- premake5.lua
+++ premake5.lua
@@ -33,7 +33,6 @@ workspace "Pong2D"
 
         files {
             "%{engine}/Source/*",
-            "%{engine}/Source/Component/*",
             "%{engine}/Source/Core/*",
             "%{engine}/Source/Misc/*",
             "%{engine}/Source/Resource/*",
