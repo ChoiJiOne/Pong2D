2a87716553ba024bfc7d14275136f7d6cbe8057f
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-11-20 15:59:34 +0900

    Camera class 구현 완료
    - 기존에 없던 월드 좌표를 윈도우 좌표로 변환하는 기능 추가

--- Engine/Source/Camera.cpp
+++ Engine/Source/Camera.cpp
@@ -1,2 +1,94 @@
 #include <Camera.h>
-#include <Math.hpp>
+
+Camera::Camera(const Vec2f& InPosition, const float& InWidth, const float& InHeight, const float& InRotate, const float& InVelocity)
+{
+	Position_ = InPosition;
+	Width_ = InWidth;
+	Height_ = InHeight;
+	Rotate_ = InRotate;
+	Velocity_ = InVelocity;
+	BoundingBox_ = Math::CalculateBoundingBox(Position_, Width_, Height_);
+}
+
+Camera::Camera(Camera&& InInstance) noexcept
+{
+	Position_ = InInstance.Position_;
+	Width_ = InInstance.Width_;
+	Height_ = InInstance.Height_;
+	Rotate_ = InInstance.Rotate_;
+	Velocity_ = InInstance.Velocity_;
+	BoundingBox_ = InInstance.BoundingBox_;
+}
+
+Camera::Camera(const Camera& InInstance) noexcept
+{
+	Position_ = InInstance.Position_;
+	Width_ = InInstance.Width_;
+	Height_ = InInstance.Height_;
+	Rotate_ = InInstance.Rotate_;
+	Velocity_ = InInstance.Velocity_;
+	BoundingBox_ = InInstance.BoundingBox_;
+}
+
+Camera& Camera::operator=(Camera&& InInstance) noexcept
+{
+	if (this == &InInstance) return *this;
+
+	Position_ = InInstance.Position_;
+	Width_ = InInstance.Width_;
+	Height_ = InInstance.Height_;
+	Rotate_ = InInstance.Rotate_;
+	Velocity_ = InInstance.Velocity_;
+	BoundingBox_ = InInstance.BoundingBox_;
+
+	return *this;
+}
+
+Camera& Camera::operator=(const Camera& InInstance) noexcept
+{
+	if (this == &InInstance) return *this;
+
+	Position_ = InInstance.Position_;
+	Width_ = InInstance.Width_;
+	Height_ = InInstance.Height_;
+	Rotate_ = InInstance.Rotate_;
+	Velocity_ = InInstance.Velocity_;
+	BoundingBox_ = InInstance.BoundingBox_;
+
+	return *this;
+}
+
+void Camera::SetPosition(const Vec2f& InPosition)
+{
+	Position_ = InPosition;
+	BoundingBox_ = Math::CalculateBoundingBox(Position_, Width_, Height_);
+}
+
+void Camera::SetWidth(const float& InWidth)
+{
+	Width_ = InWidth;
+	BoundingBox_ = Math::CalculateBoundingBox(Position_, Width_, Height_);
+}
+
+void Camera::SetHeight(const float& InHeight)
+{
+	Height_ = InHeight;
+	BoundingBox_ = Math::CalculateBoundingBox(Position_, Width_, Height_);
+}
+
+void Camera::Move(float InDeltaSeconds)
+{
+	float CosTheta = cosf(Math::ToRadian(Rotate_));
+	float SinTheta = sinf(Math::ToRadian(Rotate_));
+
+	Position_.x += (CosTheta * GetVelocity() * InDeltaSeconds);
+	Position_.y += (SinTheta * GetVelocity() * InDeltaSeconds);
+}
+
+Vec2f Camera::ConvertToWindowPosition(const Vec2f& InWorldPosition)
+{
+	return Vec2f(
+		 InWorldPosition.x - Position_.x + Width_  / 2.0f,
+		-InWorldPosition.y + Position_.y + Height_ / 2.0f
+	);
+}
--- Engine/Source/Camera.h
+++ Engine/Source/Camera.h
@@ -70,75 +70,107 @@ public:
 
 
 	/**
+	 * 2D World 내의 카메라 위치를 얻습니다.
 	 * 
+	 * @return 2D World 내의 카메라 위치를 반환합니다.
 	 */
 	Vec2f GetPosition() const { return Position_; }
 
 
 	/**
+	 * 2D World 내의 카메라 위치를 설정합니다.
 	 * 
+	 * @param InPosition - 설정할 카메라 위치입니다.
 	 */
 	void SetPosition(const Vec2f& InPosition);
 
 
 	/**
+	 * 2D World 내의 카메라 가로 크기를 얻습니다.
 	 * 
+	 * @return 2D World 내의 카메라 가로 크기를 반환합니다.
 	 */
 	float GetWidth() const { return Width_; }
 
 
 	/**
+	 * 2D World 내의 카메라 가로 크기를 설정합니다.
 	 * 
+	 * @param InWidth - 설정할 카메라의 가로 크기입니다.
 	 */
 	void SetWidth(const float& InWidth);
 
 
 	/**
+	 * 2D World 내의 카메라 세로 크기를 얻습니다.
 	 * 
+	 * @return 2D World 내의 카메라 세로 크기를 반환합니다.
 	 */
 	float GetHeight() const { return Height_; }
 
 
 	/**
-	 * 
+	 * 2D World 내의 카메라 세로 크기를 설정합니다.
+	 *
+	 * @param InHeight - 설정할 카메라의 세로 크기입니다.
 	 */
 	void SetHeight(const float& InHeight);
 
 
 	/**
+	 * 2D World 내의 카메라 뱡향 각도를 얻습니다.
 	 * 
+	 * @return 2D World 내의 카메라 방향 각도를 반환합니다.
 	 */
 	float GetRotate() const { return Rotate_; }
 
 
-
 	/**
-	 * 
+	 * 2D World 내의 카메라 뱡향 각도를 설정합니다.
+	 *
+	 * @param InRotate - 설정할 카메라의 방향 각도입니다.
 	 */
 	void SetRotate(const float& InRotate) { Rotate_ = InRotate; }
 
 
 	/**
+	 * 2D World 내의 카메라의 속력을 얻습니다.
 	 * 
+	 * @return 2D World 내의 카메라의 속력을 반환합니다.
 	 */
 	float GetVelocity() const { return Velocity_; }
 
 
 	/**
+	 * 2D World 내의 카메라의 속력을 설정합니다.
 	 * 
+	 * @param InVelocity - 설정할 카메라의 속력입니다.
 	 */
 	void SetVelocity(const float& InVelocity) { Velocity_ = InVelocity; }
 
 
 	/**
-	 * 
+	 * 초단위 델타 시간값 만큼 카메라를 움직입니다.
+	 *
+	 * @param InDeltaSeconds - 카메라를 움직일 초단위 델타 시간 값입니다.
 	 */
-	Vec2f GetWindowPosition(const Vec2f& InWorldPosition);
+	void Move(float InDeltaSeconds);
+
+
+	/**
+	 * 입력 받은 월드 좌표를 카메라를 기준으로 윈도우 좌표로 변환합니다.
+	 * 즉, 월드 좌표 => 카메라 좌표 => 윈도우 좌표 로 변환합니다.
+	 * 
+	 * @param InWorldPosition - 변환할 월드 좌표입니다.
+	 * 
+	 * @return 변환된 윈도우 좌표를 반환합니다.
+	 */
+	Vec2f ConvertToWindowPosition(const Vec2f& InWorldPosition);
 
 
 private:
 	/**
-	 * 카메라의 현재 위치입니다.
+	 * 2D 월드 상의 카메라 현재 위치입니다.
 	 */
 	Vec2f Position_;
 
