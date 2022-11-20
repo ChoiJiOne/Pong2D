99a563443f2393b952f424b0f1b4673d9f9a591c
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-11-20 15:59:45 +0900

    SpriteRenderComponent class 선언 및 구현

--- Game/Source/SpriteRenderComponent.cpp
+++ Game/Source/SpriteRenderComponent.cpp
@@ -1,9 +1,21 @@
 #include "SpriteRenderComponent.h"
 
+#include <RigidBodyComponent.h>
+#include <ContentManager.h>
+#include <Text.hpp>
 #include <GameObject.h>
 #include <Renderer.h>
 #include <Camera.h>
 
 void SpriteRenderComponent::Tick(Renderer& InRenderer, Camera& InCamera)
 {
+	RigidBodyComponent* RigidBody = GetGameObject()->GetComponent<RigidBodyComponent>(Text::GetHash("body"));
+	Texture& SpriteTexture = ContentManager::Get().GetTexture(SpriteKey_);
+
+	InRenderer.DrawTexture2D(
+		SpriteTexture,
+		Math::ConvertPixelCoordinate(InCamera.ConvertToWindowPosition(RigidBody->GetPosition())),
+		static_cast<int32_t>(RigidBody->GetWidth()),
+		static_cast<int32_t>(RigidBody->GetHeight())
+	);
 }
